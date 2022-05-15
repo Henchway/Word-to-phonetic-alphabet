49e43131d7aa92737afbfa438846927087fef3f8
Thomas Scheibelhofer <thomas.scheibelhofer@live.at>
2022-05-15 19:51:48 +0200

    updated

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-import json
+alphabets = {
+    "ger": {
+        "A": "Anton",
+        "Ä": "Ärger",
+        "B": "Berta",
+        "C": "Cäsar",
+        "D": "Dora",
+        "E": "Emil",
+        "F": "Friedrich",
+        "G": "Gustav",
+        "H": "Heinrich",
+        "I": "Ida",
+        "J": "Julius",
+        "K": "Konrad",
+        "L": "Ludwig",
+        "M": "Martha",
+        "N": "Nordpol",
+        "O": "Otto",
+        "Ö": "Österreich",
+        "P": "Paula",
+        "Q": "Quelle",
+        "R": "Richard",
+        "S": "Siegfried",
+        "ß": "scharfes S",
+        "T": "Theodor",
+        "U": "Ulrich",
+        "Ü": "Übel",
+        "V": "Viktor",
+        "W": "Wilhelm",
+        "X": "Xanthippe / Xaver",
+        "Y": "Ypsilon",
+        "Z": "Zürich / Zeppelin"
+    }
+}
 
 
 def read_sentence():
@@ -9,15 +42,15 @@ def read_sentence():
 
 def generate_phonetic_alphabet(sentence_input: str, language_input: str):
     result = ""
-    with open(f'./alphabets/{language_input.lower()}.json') as f:
-        data = json.load(f)
+    data = alphabets[language_input]
     words = sentence_input.split(" ")
     for word in words:
         result += f"{word}:\t "
         for character in word:
-            if character.upper() in data['alphabet'].keys():
-                result += data['alphabet'][character.upper()] + " "
-        result += "\r"
+            if character.upper() in data.keys():
+                result += data[character.upper()] + " "
+        result += "\n"
+    return result
 
 
 if __name__ == '__main__':
