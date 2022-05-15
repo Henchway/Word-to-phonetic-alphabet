9134294f4a4f4bb54cb1a5b5f74a416fd255d476
Thomas Scheibelhofer <thomas.scheibelhofer@live.at>
2022-05-15 18:53:04 +0200

    test

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import json
 
+
 def read_sentence():
     sentence_input = input("Please type the word: ")
     language_input = input("Please specify the language: ") or "ger"
     return sentence_input, language_input
 
 
-def write_phonetic_alphabet(sentence_input: str, language_input: str):
+def generate_phonetic_alphabet(sentence_input: str, language_input: str):
+    result = ""
     with open(f'./alphabets/{language_input.lower()}.json') as f:
         data = json.load(f)
     words = sentence_input.split(" ")
     for word in words:
-        print(f"{word}:\t", end=" ")
+        result += f"{word}:\t "
         for character in word:
             if character.upper() in data['alphabet'].keys():
-                print(data['alphabet'][character.upper()], end=" ")
-        print("\r")
-
+                result += data['alphabet'][character.upper()] + " "
+        result += "\r"
 
 
 if __name__ == '__main__':
     sentence, language = read_sentence()
-    write_phonetic_alphabet(sentence, language)
+    generate_phonetic_alphabet(sentence, language)
